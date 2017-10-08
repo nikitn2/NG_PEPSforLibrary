89895656c186e23488302da140fd230a6a73a799
Nik Gourianov <nikitn@hotmail.com>
2017-10-08 18:00:41 +0100

    Initial Commit

---
+++ NG_PEPSforLibrary/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NG_PEPSforLibrary
+//
+//  Created by Nik Gourianov on 08.10.2017.
+//  Copyright © 2017 Nik Gourianov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
